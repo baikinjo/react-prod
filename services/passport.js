cfd221e673dde81aa3a7b2bd0e17c1dfe7fa5d83
Injo Baik <baikinjo.28@gmail.com>
2017-11-23 20:26:54 -0800

    re-pushed all sources due to an commit issue

--- services/passport.js
+++ services/passport.js
@@ -6,34 +6,28 @@ const keys = require('../config/keys');
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-    User
-        .findById(id)
-        .then(user => {
-            done(null, user);
-        });
+  User
+    .findById(id)
+    .then(user => {
+      done(null, user);
+    });
 });
 
 passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-    proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-    User
-        .findOne({ googleId: profile.id })
-        .then((existingUser) => {
-            if (existingUser) {
-                // we already have a record with the given profile ID
-                done(null, existingUser);
-            } else {
-                // we don't have a user record with this ID, make a new record
-                new User({ googleId: profile.id })
-                    .save()
-                    .then(user => done(null, user));
-            }
-        });
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback',
+  proxy: true
+}, async(accessToken, refreshToken, profile, done) => {
+  const existingUser = await User.findOne({googleId: profile.id});
+  if (existingUser) {
+    return done(null, existingUser);
+  }
+  const user = await new User({googleId: profile.id}).save();
+  done(null, user);
+
 }));
